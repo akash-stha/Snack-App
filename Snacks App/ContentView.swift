bf85c4a74b0059d2ce72638c532a20268ed9fc72
Akash Shrestha <aakashtha49@gmail.com>
2024-08-17 23:51:20 -0400

    Initial Commit

---
+++ Snacks App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snacks App
+//
+//  Created by Akash Shrestha on 2024-08-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snacks App/Snacks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snacks_AppApp.swift
+//  Snacks App
+//
+//  Created by Akash Shrestha on 2024-08-17.
+//
+
+import SwiftUI
+
+@main
+struct Snacks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
